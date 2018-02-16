39f8b3e51adbc579eae934a3428958b96ae879d1
Aman Kumar Singh <er.aks473@gmail.com>
2018-02-17 01:10:23 +0530

    Add Accessiblity Service

---
+++ app/src/main/java/com/example/android/materialpicker/WAAccessibilityService.java
@@ -0,0 +1,40 @@
+package com.example.android.materialpicker;
+
+import android.accessibilityservice.AccessibilityService;
+import android.view.accessibility.AccessibilityEvent;
+import android.widget.Toast;
+
+/**
+ * Created by Aman on 17-02-2018.
+ */
+
+public class WAAccessibilityService extends AccessibilityService {
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+
+        final int eventType = event.getEventType();
+        String eventText = null;
+        switch(eventType) {
+            case AccessibilityEvent.TYPE_VIEW_CLICKED:
+                eventText = "Clicked: ";
+                break;
+            case AccessibilityEvent.TYPE_VIEW_FOCUSED:
+                eventText = "Focused: ";
+                break;
+        }
+
+        eventText = eventText + event.getContentDescription();
+
+        // Do something nifty with this text, like speak the composed string
+        // back to the user.
+        Toast.makeText(this, eventText, Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onInterrupt() {
+
+
+    }
+
+}
